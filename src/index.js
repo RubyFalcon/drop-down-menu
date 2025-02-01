903a941c955b24ed837d446d2de9885002c635ae
Tupay Felkin <61619620+RubyFalcon@users.noreply.github.com>
2025-02-01 22:05:31 +0000

    dropdown menu added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import "./styles.css";
 import { homePage } from "./home";
+const dropbtn = document.querySelector(".dropbtn")
+dropbtn.addEventListener("click", ()=> myFunction())
 
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+ 
 homePage();
-console.log("Hello world");
